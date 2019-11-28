8defdf96aeb925f82f5a243272797bdf4e299856
Victor Javier <VictorJavierGuamnChulunchana@gmail.com>
2019-11-28 20:11:33 +0000

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-
-
-
-
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Hello } from "./components/hello/Hello";
-
+import { NumberPicker } from "./components/numberPicker/NumberPicker";
 class App extends React.Component<{}, {}> {
   render() {
-    return <Hello name="Pepe"/>;
+    return (
+      <div>
+        <Hello name="Pepe"></Hello>
+        <NumberPicker />
+      </div>
+    );
   }
 }
 
